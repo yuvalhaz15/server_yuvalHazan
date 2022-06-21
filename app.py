829a655fe0752a3c8981a080e5df5dd7186a85a0
yuvalhaz15 <yuvalhaz@post.bgu.ac.il>
2022-06-21 10:44:40 +0300

    :)

--- app.py
+++ app.py
@@ -8,18 +8,23 @@ app = Flask(__name__)
 app.secret_key = '123'
 app.config['SESSION_PERMANENT'] = True
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=20)
+#
+# @app.route('/')
+# def first_page():  # put application's code here
+#     return redirect(url_for('display_home_page'))
+#
+# @app.route('/home')
+# def display_home_page():  # put application's code here
+#     return render_template('homePage.html')
+#
+# @app.route('/conact')
+# def display_conact_us():  # put application's code here
+#     return render_template('conactUs.html')
 
-@app.route('/')
-def first_page():  # put application's code here
-    return redirect(url_for('display_home_page'))
+@app.route('/assignment3_1')
+def display_hobbies_page():  # put application's code here
+    return render_template('assignment3_1.html')
 
-@app.route('/home')
-def display_home_page():  # put application's code here
-    return render_template('homePage.html')
-
-@app.route('/conact')
-def display_conact_us():  # put application's code here
-    return render_template('conactUs.html')
 
 if __name__ == '__main__':
     app.run()
