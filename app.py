520f416f07d6753c7ff698e25cee3f6f57f25970
yuvalhaz15 <yuvalhaz@post.bgu.ac.il>
2022-06-21 17:19:27 +0300

    :)

--- app.py
+++ app.py
@@ -33,5 +33,68 @@ def display_hobbies_page():
                            no_hobbies_message='no hobbies to display' )
 
 
+
+# @app.route('/open_users')
+# def open_users_page():
+#     render_template('assignment3_2.html')
+
+
+users = {
+     "user1": {"name": "Edinson", "email": "Cavani@gmail.com", "user_name": "El metador" },
+    "user2": {"name": "Cristiano", "email": "CR7@gmail.com", "user_name": "CR7"},
+    "user3": {"name": "Bruno", "email": "Bruno@gmail.com", "user_name": "BR"},
+    "user4": {"name": "Messi", "email": "Messi@gmail.com", "user_name": "The flea"},
+    "user5": {"name": "Kobe", "email": "kobe@gmail.com", "user_name": "The black mamba"}
+}
+user_data= {
+    'El metador': '1234',
+    'CR7': '1235',
+    'BR': '1236',
+    'The flea': '1237',
+    'The black mamba': '1238',
+}
+
+
+@app.route('/assignment3_2', methods=['GET', 'POST'])
+def display_users_page():  # put application's code here
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username in user_data:
+            user_password = user_data[username]
+            if user_password == password:
+                session['username'] = username
+                session['logedin'] = True
+                return render_template('assignment3_2.html',
+                                       message='Success',
+                                       username=username)
+            else:
+                return render_template('assignment3_2.html',
+                                       message='Incorrect password!')
+        else:
+            return render_template('assignment3_2.html',
+                                   message='Please sign in!')
+
+    if 'name' in request.args:
+        name = request.args["name"]
+        if name == '':
+            return render_template('assignment3_2.html',users=users)
+        details = None
+        for user_name in users.values():
+            if user_name['name'] == name:
+                details = user_name
+                break
+        if details:
+            return render_template('assignment3_2.html',
+                                   name=details['name'],
+                                   email=details['email'],
+                                   user_name=details['user_name'])
+        else:
+            return render_template('assignment3_2.html',
+                                   message2='No user found, sorry about that')
+    return render_template('assignment3_2.html',
+                           users=users)
+
+
 if __name__ == '__main__':
     app.run()
